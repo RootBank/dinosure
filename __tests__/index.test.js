3f597fba95bfee981691976e31e1d85f97b9cd71
Nick Cuthbert <nick@cuthbert.co.za>
2017-11-18 00:37:39 +0200

    Making progress

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,19 @@
 /* eslint-env jest */
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import { shallow, configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import App from '../pages/index.js';
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
-describe('With Enzyme', () => {
-  it('App shows "Hello world!"', () => {
-    const app = shallow(<App />);
+// describe('With Enzyme', () => {
+//   it('App shows "Hello world!"', () => {
+//     const app = shallow(<App />);
 
-    expect(app.find('p').text()).toEqual('Hello World!');
-  });
-});
+//     expect(app.find('p').text()).toEqual('Hello World!');
+//   });
 
+// });
 describe('With Snapshot Testing', () => {
   it('App shows "Hello world!"', () => {
     const component = renderer.create(<App />);
