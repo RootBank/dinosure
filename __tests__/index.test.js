74fe6c5dbe4eb5b1f9d7a12c00dcd90c8a960930
Nick Cuthbert <nick@cuthbert.co.za>
2017-11-18 11:43:01 +0200

    Removed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,23 +1,23 @@
-/* eslint-env jest */
-import React from 'react';
-// import { shallow, configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
-import App from '../pages/index.js';
-// configure({ adapter: new Adapter() });
+// /* eslint-env jest */
+// import React from 'react';
+// // import { shallow, configure } from 'enzyme';
+// // import Adapter from 'enzyme-adapter-react-16';
+// import renderer from 'react-test-renderer';
+// import App from '../pages/index.js';
+// // configure({ adapter: new Adapter() });
 
-// describe('With Enzyme', () => {
-//   it('App shows "Hello world!"', () => {
-//     const app = shallow(<App />);
+// // describe('With Enzyme', () => {
+// //   it('App shows "Hello world!"', () => {
+// //     const app = shallow(<App />);
 
-//     expect(app.find('p').text()).toEqual('Hello World!');
-//   });
+// //     expect(app.find('p').text()).toEqual('Hello World!');
+// //   });
 
-// });
-describe('With Snapshot Testing', () => {
-  it('App shows "Hello world!"', () => {
-    const component = renderer.create(<App />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
+// // });
+// // describe('With Snapshot Testing', () => {
+// //   it('App shows "Hello world!"', () => {
+// //     const component = renderer.create(<App />);
+// //     const tree = component.toJSON();
+// //     expect(tree).toMatchSnapshot();
+// //   });
+// // });
