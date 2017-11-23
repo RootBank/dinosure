efddc160c8e3a5f9f49a51f226b23777df44666c
Jacques <concept.prototyping@gmail.com>
2017-11-23 19:00:54 +0200

    About page copy changed

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,24 @@
 import page from '../components/page';
 
 export default page(() =>
-  <section className='section' >
+  <section className='section'>
     <div className='container content'>
-      <h1 className='title'>About</h1>
+      <h1 className='title has-text-centered'>Who are we?</h1>
       <p>
-          Dinosure was founded by Dennis Nedry in 2005. Alistar has long held the deep seated belief that his mother was tragically taken by a pack of velociraptors<sup><a href='#fn1' id='ref1'>1</a></sup>.
-          Dinosure began as a way to ensure that no one else had to experience the same loss.
+          Hero was founded by Jowyk Muller, Kosie Jansen van Rensburg and Jacques de Waal. It all started with Jowyk having a son, little Jowan. As an actuary, he thought he would just quickly go to one of the reputable insurers in South Africa, fill in some details and get his life insurance sorted. How difficult could it be right? Well, in a word, ridiculous. His life has changed, but not his insurance. The whole process seems like a mountain to him. If this whole process is difficult to him, an actuary working in the industry, how difficult mustn't it be for someone like Anandi Ellis, a Designer and recent Mom? She purchased a life insurance policy for her little princess, but just sees it as one of those things that she signed at the time. She doesn't even want to understand, because insurance is just explained so poorly.
       </p>
       <p>
-          Since then Dinosure has steadily grown to offer a diverse range of services. Our many satisfied customers now extend beyond those with simply dinosaur
-          related concerns, and also include those who want protection from other creatures such as dragons, gorgons and manticores.
+          We are people that believe that given independant, in other words not getting advice from someone that will get a huge commission when you buy a certain product, relevant advice, each person can decide for themselves what to do with their money so that they and their family are better off tomorrow... and the next day... and the next day.
       </p>
-      <p>
-          The insurance component of Dinosure now operates on the <a href='http://root.co.za'><img style={{ transform: 'translate(0, 15%)', height: '1rem' }} src='https://storage.googleapis.com/root-bank/artwork/logos/dark-300x94.png' /></a> platform.
-          This provides a robust foundation for serving the insurance needs of our customers faster and better.
-      </p>
-      <sup id='fn1'>1. Despite what the authorities might think<a href='#ref1' title='Jump back to footnote 1 in the text.'>↩</a></sup>
+      <h4>
+        We believe that people's own money is so personal that you can't outsource that to someone else. It's time to stop sticking our heads in the sand and start taking responsibility for our own money. It is our money, we worked hard for it. Why would we not want to understand what we do with it?
+      </h4>
+      <h3>
+        We believe that your finances should not be that hard. You should be able to understand it, when it is a conversation in plain english, without technical jargon, and without making you feel like an idiot.
+      </h3>
+      <h2>
+        We believe that people that are hungry to change the world will change the world. The Hero community is busy changing the world. Are you?
+      </h2>
     </div>
   </section>
 );
