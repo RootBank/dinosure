98962d870fca9451eb2f7d9f72d5453246794cab
Nick Cuthbert <nick@cuthbert.co.za>
2017-11-24 17:26:22 +0200

    Finished flow

--- pages/policy-issued.js
+++ pages/policy-issued.js
@@ -1,1 +1,11 @@
-export default () => (<div>Success</div>);
+export default () => page(({reason}) =>
+  <section className='section'>
+    <div className='container'>
+      <h1 className='title is-3'>Hooray!</h1>
+      <p> You've just been issued a new dinosure policy!
+          Please check your email: it contains your policy documents, along with
+          instructions on how to set your beneficiaries and create a profile which will allow you to
+          manage your policy.
+      </p>
+    </div>
+  </section>);
