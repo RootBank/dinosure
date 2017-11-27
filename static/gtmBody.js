53a780b260afb4f622226c0b3a90ff0e10221a73
Jacques <concept.prototyping@gmail.com>
2017-11-27 14:14:50 +0200

    fixed push errors

--- static/gtmBody.js
+++ static/gtmBody.js
@@ -1,1 +1,1 @@
-<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4FNPB7" height="0" width="0" style="display: none; visibility: hidden"></iframe>
+<iframe src='https://www.googletagmanager.com/ns.html?id=GTM-P4FNPB7' height='0' width='0' style='display: none; visibility: hidden'></iframe>;
