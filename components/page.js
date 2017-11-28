a9b8715f98cad95681a2e6dfd2f5ad47f5898a08
Jacques <concept.prototyping@gmail.com>
2017-11-28 14:57:27 +0200

    fixed push errors

--- components/page.js
+++ components/page.js
@@ -27,7 +27,6 @@ export default (Page, { footer: AuxiliaryFooter, getInitialProps, datastores = {
       datastores[store].subscribe(x => this.setState({ [store]: x }))
     );
     this.setState({ currentPage: window.location.pathname });
-
   }
 
   componentWillUnmount () {
