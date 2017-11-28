0e90e5b3a25382cf9b4b3ccbbb465178b7b391f1
Jacques <concept.prototyping@gmail.com>
2017-11-28 15:38:09 +0200

    fixed push errors

--- pages/claim/1.js
+++ pages/claim/1.js
@@ -1,5 +1,4 @@
 import page from '../../components/page';
-import FormFooter from '../../components/form-progress';
 
 export default page(() =>
   <section className='section '>
@@ -7,5 +6,5 @@ export default page(() =>
       <h1 className='title'>Your claim has been submitted</h1>
       <p>Thabo, our claims consultant will email you shortly. Please keep an eye on your inbox as this is how we will communicate with you.</p>
     </div>
-  </section>,
+  </section>
 );
