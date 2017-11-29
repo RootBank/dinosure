af8e81a98410fdf87d260bd87a46408d2045113b
Nick Cuthbert <nick@cuthbert.co.za>
2017-11-29 17:50:40 +0200

    Thingz

--- pages/policy-issued.js
+++ pages/policy-issued.js
@@ -12,11 +12,12 @@ export default page(class extends React.Component {
     return <section className='section'>
       <div className='container content has-text-centered'>
         <h1 className='title is-3'>Hooray!</h1>
-        <h3 className='subtitle'>You are now part of the Hero cummunity!</h3>
+        <h3 className='subtitle'>You are now part of the Hero community!</h3>
         <div className='is-divider' data-content='Next Steps' />
         <p>You have a very important email waiting for you in your inbox. The email contains important attachments and a link to activate your account.
         </p>
-        <p>If you should die, it can be very difficult for your family to access the money if you haven't chosen a beneficiary. Make it easy for them and click on the link in the email.</p>
+        <p><em>If you need any help, please call us on </em></p>
+        <h3 className='is-3 has-text-centered'>087 550 4246</h3>
       </div>
     </section>;
   }
