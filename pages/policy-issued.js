e58b2099dec0875b2ee5e2821a9d53505d2e28b2
Jason <jason@root.co.za>
2018-02-02 13:54:55 +0200

    Policy issued text

--- pages/policy-issued.js
+++ pages/policy-issued.js
@@ -39,8 +39,7 @@ export default page(class extends React.Component {
           <h1 className='title is-3'>Hooray!</h1>
           <p> You've just been issued a new dinosure policy!
               Please check your email: it contains your policy documents, along with
-              instructions on how to set your beneficiaries and create a profile which will allow you to
-              manage your policy.
+              instructions on how to minimise your risk of a dinosaur attack.
           </p>
         </div>
       </section>;
