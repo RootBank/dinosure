425e7f57b4769e68748dd3b4f98c45d3c9041d59
Jason <jason@root.co.za>
2018-02-06 16:29:22 +0200

    Phone number validation

--- pages/checkout/profile/cellphone.js
+++ pages/checkout/profile/cellphone.js
@@ -8,17 +8,17 @@ import applicationStore from '../../../datastores/application';
 import Steps from '../../../components/checkout-steps';
 import { asYouType, isValidNumber, parse } from 'libphonenumber-js';
 import Input from '../../../components/input';
+import phone from 'phone';
 
 const setPhoneNumber = (event) => {
   const cellphone = new asYouType('ZA').input(event.target.value);
   applicationStore.update(state => ({ ...state, cellphone }));
 };
 
-const isCellphoneNumberValid = (value) =>
-  isValidNumber(parse(value, 'ZA'));
+const isCellphoneNumberValid = (value) => isValidNumber(parse(value, 'ZA')) && phone(value, 'ZAF').length > 0;
 
 export default page(class extends React.Component {
-  render () {
+  render() {
     const application = this.props.application;
     return <section className='section'>
       <div className='columns'>
@@ -27,7 +27,7 @@ export default page(class extends React.Component {
           <h3 className='title is-3'>My cellphone number is</h3>
           <div className='columns'>
             <div className='column' />
-            <div style={{margin: '0.6em'}} className='column'>
+            <div style={{ margin: '0.6em' }} className='column'>
               <Input autoFocus={true} style={{ width: '24rem', textAlign: 'center' }} onChange={setPhoneNumber} className='input title column is-medium' type='text' placeholder='' value={application.cellphone} />
             </div>
             <div className='column' />
@@ -38,19 +38,19 @@ export default page(class extends React.Component {
     </section>;
   }
 }, {
-  footer: ({application}) =>
-    <div>
-      <section className='section'>
-        <div className='level form-nav'>
-          <div className='level-item'>
-            <Link href='/checkout/profile/email'><button className='button is-primary is-inverted'><a>Prev</a></button></Link>
-            <Link href='/checkout/questions'><button id='submit' className='button is-primary' disabled={!isCellphoneNumberValid(application.cellphone)}>Next</button></Link>
+    footer: ({ application }) =>
+      <div>
+        <section className='section'>
+          <div className='level form-nav'>
+            <div className='level-item'>
+              <Link href='/checkout/profile/email'><button className='button is-primary is-inverted'><a>Prev</a></button></Link>
+              <Link href='/checkout/questions'><button id='submit' className='button is-primary' disabled={!isCellphoneNumberValid(application.cellphone)}>Next</button></Link>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className='section'>
-        <Steps currentStep={0} />
-      </section>
-    </div>,
-  datastores: { quote: quoteStore, application: applicationStore }
-});
+        </section>
+        <section className='section'>
+          <Steps currentStep={0} />
+        </section>
+      </div>,
+    datastores: { quote: quoteStore, application: applicationStore }
+  });
