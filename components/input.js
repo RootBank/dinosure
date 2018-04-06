c5b92a4bf89e158bd7689ebd858778ab29e3d89e
Nick Cuthbert <nick@cuthbert.co.za>
2018-04-06 18:43:21 +0200

    Removed yarn lockfile

--- components/input.js
+++ components/input.js
@@ -10,7 +10,7 @@ export default class Input extends React.Component {
   }
 
   componentDidUpdate() {
-    if(this.refs.input === document.activeElement) {
+    if (this.refs.input === document.activeElement) {
       this.refs.input.setSelectionRange(this.state.curPos, this.state.curPos);
     }
   }
@@ -31,23 +31,11 @@ export default class Input extends React.Component {
   };
 
   render() {
-    if(this.props.autoFocus) {
+    if (this.props.autoFocus) {
       return <input
-          className={this.props.className}
-          disabled={this.props.disabled}
-          autoFocus={!!this.props.autoFocus}
-          style={this.props.style}
-          onChange={this._onChange}
-          value={this.props.value}
-          placeholder={this.props.placeholder}
-          type={this.props.type}
-          ref='input'
-        />;
-    }
-    
-    return <input
         className={this.props.className}
         disabled={this.props.disabled}
+        autoFocus={!!this.props.autoFocus}
         style={this.props.style}
         onChange={this._onChange}
         value={this.props.value}
@@ -55,5 +43,17 @@ export default class Input extends React.Component {
         type={this.props.type}
         ref='input'
       />;
+    }
+
+    return <input
+      className={this.props.className}
+      disabled={this.props.disabled}
+      style={this.props.style}
+      onChange={this._onChange}
+      value={this.props.value}
+      placeholder={this.props.placeholder}
+      type={this.props.type}
+      ref='input'
+    />;
   }
 }
